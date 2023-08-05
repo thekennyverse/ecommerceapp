da1fcd8ed8506c0f09c47ae644186d702543653e
Kenneth Severe <kennethsevere916@gmail.com>
2023-08-05 12:59:23 -0400

    search bar working

---
+++ views/search.js
@@ -0,0 +1,12 @@
+document.getElementById('searchForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const searchQuery = document.getElementById('searchInput').value;
+    const host = "localahost:4000"
+    const url = host + "/products/search/" + encodeURIComponent(searchQuery);
+    
+    const response = await fetch(url)
+    const data = await response.json()
+
+    console.log(data);
+  });
